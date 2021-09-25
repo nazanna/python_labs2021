309efffe80b88601fbea0224176899499b16c608
hasoart <artash.gyolecyan@gmail.com>
2021-09-25 21:50:31 +0300

    Added realistic physics

--- Turtle/part2/gas.py
+++ Turtle/part2/gas.py
@@ -1,52 +1,55 @@
-import random
-import turtle
+import numpy as np
+import turtle as tt
 
-number_of_turtles = 20
+number_of_turtles = 5
 
+pool = [tt.Turtle(shape='circle') for _ in range(number_of_turtles)]
+r = np.random.normal(loc=0, scale=50, size=(number_of_turtles, 2))
+v = np.random.normal(loc=0, scale=10, size=(number_of_turtles, 2))
+dt = 1
 
-pool = [turtle.Turtle(shape='circle') for _ in range(number_of_turtles)]
-vx   = [random.random() * 100 - 50 for _ in range(number_of_turtles)]
-vy   = [random.random() * 100 - 50 for _ in range(number_of_turtles)]
-x    = [random.randint(-200, 200)  for _ in range(number_of_turtles)]
-y    = [random.randint(-200, 200)  for _ in range(number_of_turtles)]
+box_size = 200
+hitbox_size = 20
 
-turtle.penup()
-turtle.speed(0)
-turtle.goto(-200, -200)
-turtle.pendown()
-turtle.goto(-200, 200)
-turtle.goto(200, 200)
-turtle.goto(200, -200)
-turtle.goto(-200, -200)
-turtle.ht()
+tt.speed(0)
+tt.penup()
+tt.goto(-box_size, -box_size)
+tt.pendown()
+tt.forward(2 * box_size)
+tt.left(90)
+tt.forward(2 * box_size)
+tt.left(90)
+tt.forward(2 * box_size)
+tt.left(90)
+tt.forward(2 * box_size)
+
+tt.ht()
 
 for i, unit in enumerate(pool):
     unit.penup()
     unit.speed(0)
-    unit.goto(x[i], y[i])
+    unit.goto(*r[i])
 
-dt = 0.1
 while True:
-    for i in range(number_of_turtles):
-        x[i] += vx[i] * dt
-        y[i] += vy[i] * dt
-
-        if x[i] >= 200 or x[i] <= -200:
-            x[i] = 400 * (x[i] >= 200) - 200
-            vx[i] *= -1
-
-        if y[i] >= 200 or y[i] <= -200:
-            y[i] = 400 * (y[i] >= 200) - 200
-            vy[i] *= -1
-
-        pool[i].goto(x[i], y[i])
+    r += v * dt
 
     for i in range(number_of_turtles):
-        for j in range(i, number_of_turtles):
-            if (x[i] - x[j])*(x[i] - x[j]) + \
-               (y[i] - y[j])*(y[i] - y[j]) <= 100:
-                vx[i] *= -1
-                vy[i] *= -1
+        for dim in (0, 1):
+            if np.abs(r[i][dim]) >= box_size:
+                r[i][dim] = box_size * np.sign(r[i][dim])
+                v[i][dim] *= -1
 
-                vx[j] *= -1
-                vy[j] *= -1
+        pool[i].goto(*r[i])
+
+    for i in range(number_of_turtles):
+        for j in range(i + 1, number_of_turtles):
+            if np.linalg.norm(r[i] - r[j]) < hitbox_size:
+                v1, x1 = v[i], r[i]
+                v2, x2 = v[j], r[j]
+                dx = x1 - x2
+                dv = v1 - v2
+                ldx = np.linalg.norm(dx) ** 2
+                dvx = np.dot(dv, dx)
+                v[i] = v1 - dx * dvx / ldx
+                v[j] = v2 + dx * dvx / ldx
+                r[i] = r[j] + hitbox_size * dx / np.linalg.norm(dx)
