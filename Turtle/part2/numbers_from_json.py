d815a51bd5c970bbfdae114e11ea47430d754b04
hasoart <artash.gyolecyan@gmail.com>
2021-09-25 20:28:12 +0300

    added json support

--- Turtle/part2/numbers_from_json.py
+++ Turtle/part2/numbers_from_json.py
@@ -1,40 +1,35 @@
 import json
 import turtle
 
-# 1  2
-# 3  4
-# 5  6
-
-
-
-def draw(number, side=40):
-    turtle.penup()
+def draw_from_coords(font_size, coords_list):
     x0, y0 = turtle.pos()
 
-    coords = [(x0, y0           ), (x0 + side, y0           ),
-              (x0, y0 -     side), (x0 + side, y0 -     side),
-              (x0, y0 - 2 * side), (x0 + side, y0 - 2 * side)]
-
-    def goto(cell):
-        turtle.goto(*coords[cell - 1])
-
-    goto(number[0])
-    turtle.pendown()
-
-    for el in number:
-        goto(el)
+    def goto(r_rel):
+        x_rel, y_rel = r_rel
+        turtle.goto(x0 + x_rel * font_size, y0 + y_rel * font_size)
 
     turtle.penup()
-    goto(1)
+    goto(coords_list[0])
+    turtle.pendown()
+    for coord in coords_list:
+        goto(coord)
+
+    turtle.penup()
+    turtle.goto(x0, y0)
     turtle.pendown()
 
 with open('font.json', 'r') as f:
 	font = json.load(f)
 
-number = input()
+font_size = font["font_size"]
+spacing = font["spacing"]
 
-for i in number:
-    draw(font[i], side=font['font size'])
+turtle.penup()
+turtle.goto(-400, 0)
+turtle.pendown()
+
+for i in "0123456789ծ":
+    draw_from_coords(font_size, font[i])
     turtle.penup()
-    turtle.forward(font['font size'] * 1.5)
+    turtle.forward(spacing)
     turtle.pendown()
